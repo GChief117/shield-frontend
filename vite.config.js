be7aa493b78d3c2fa29b6a6d3c24133cc3adb4b6
Gunnar Beck Nelson <52630999+GChief117@users.noreply.github.com>
2026-01-10 20:06:13 -0500

    Add path alias resolution in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   base: '/shield-frontend/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
